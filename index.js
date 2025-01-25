53e1205713e8ee408f35f563f59c2fb7f0e1e5d8
arnavthorat78 <arnavthorat78@gmail.com>
2025-01-25 11:21:41 +1100

    Finished filename extractor

---
+++ index.js
@@ -0,0 +1,23 @@
+import chalk from "chalk";
+import fs from "fs";
+
+const folderInput = process.argv[2];
+const fileOutput = process.argv[3];
+
+if (!folderInput || !fileOutput) {
+  console.log(chalk.bold.red("Please provide a file and folder as an argument.\n"));
+  process.exit(1);
+} else if (!fs.existsSync(folderInput)) {
+  console.log(chalk.bold.red("Folder does not exist.\n"));
+  process.exit(1);
+} else if (!fs.statSync(folderInput).isDirectory()) {
+  console.log(chalk.bold.red("Path is a file, expected directory.\n"));
+  process.exit(1);
+}
+
+const files = fs.readdirSync(folderInput);
+const fileNames = files.map((file) => file.replace(/\.[^/.]+$/, ""));
+
+fs.writeFileSync(fileOutput, fileNames.join("\n"));
+
+console.log(chalk.green(`File names have been written to ${fileOutput}.\n`));
